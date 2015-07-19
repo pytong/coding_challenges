6b1685e8da235bbced16caffaf891aa951ebdb03
Puiyee Tong <puiyee@telmate.com>
2015-07-19 14:09:54 -0400

    Added odd_numbers.rb

---
+++ ruby/odd_numbers.rb
@@ -0,0 +1,19 @@
+# Challenge Description:
+# https://www.codeeval.com/open_challenges/25
+
+# Print the odd numbers from 1 to 99.
+
+# INPUT SAMPLE:
+
+# There is no input for this program.
+
+# OUTPUT SAMPLE:
+
+# Print the odd numbers from 1 to 99, one number per line.
+
+
+for i in 1..99 do
+  if i % 2 == 1
+    puts i
+  end
+end
